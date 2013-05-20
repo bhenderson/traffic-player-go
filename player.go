77e996b2bfe69e6fc62d45011de6a4ba24e90892
bhenderson <bhenderson@yp.com>
2013-05-20 08:33:50 -0700

    Print 10%

--- player.go
+++ player.go
@@ -10,31 +10,9 @@ import (
 
 type lineMap map[string](chan string)
 
-type broadcaster struct {
-    sender interface{}
-    recievers [](chan string)
-}
-
-func (b *broadcaster) makeSender(c interface{}) {
-    b.sender = c
-    if !b.recievers {
-        b.recievers = make([](chan string))
-    }
-    go func() {
-        for {
-            msg := <-b.sender
-            for rec := range b.recievers {
-                go func() { rec <- msg }()
-            }
-        }
-    }()
-    return b.sender
-}
-
-func (b *broadcaster) makeRec() (chan string) {
-    c := make(chan string)
-    b.recievers = append(b.recievers, c)
-    return c
+type ticker struct {
+    sender <-chan time.Time
+    recievers [](chan time.Time)
 }
 
 /*
@@ -50,23 +28,26 @@ func (b *broadcaster) makeRec() (chan string) {
 func main() {
     lm := make(lineMap)
     in := reader()
+    done := make(chan string)
 
-    s  := make(broadcaster)
-    s.makeSender(time.Ticker)
-
-    for {
-        msg, ok := <-in
-        if !ok {
-            break
-        }
-        go prefix(lm, msg)
+    for msg := range in {
+        go prefix(lm, msg, done)
     }
 
-    time.Sleep(time.Second * 3)
+    go func() {
+        for name := range done {
+            delete(lm, name)
+        }
+    }()
+
+    // sleep until they are all done
+    for len(lm) > 0 {
+        time.Sleep(time.Second)
+    }
 }
 
 // per msg
-func prefix(lm lineMap, msg string) {
+func prefix(lm lineMap, msg string, done chan string) {
     pre := msg[:4]
 
     rec, ok := lm[pre]
@@ -74,24 +55,48 @@ func prefix(lm lineMap, msg string) {
     if !ok {
         rec = make(chan string)
         lm[pre] = rec
-        go scale(rec)
+        go scale(pre, rec, done)
     }
 
-    rec <- msg[5:]
+    rec <- msg
 }
 
 // per type
 // keep track of how many per second you get.
 // then replay back a percentage of that.
-func scale(rec chan string) {
+// the "per second" doesn't have to be synced with other types as it averages
+// out over time. Also, remove rec from lineMap if not received for a while.
+func scale(name string, rec chan string, done chan string) {
+    count := 0
+    t := time.Tick(time.Second)
+
+    for {
+        select {
+        case msg := <-rec:
+            // print 10% of the messages
+            if count % 10 == 0 {
+                printMsg(count, msg)
+            }
+            count += 1
+        case <-t:
+            // close if none received
+            if count == 0 {
+                done <- name
+                fmt.Println(name, "is done")
+                break
+            }
+            count = 0
+        }
+    }
 }
 
-func printMsg(msg string) {
+func printMsg(msgs... interface{}) {
     // amt := time.Second * time.Duration(rand.Intn(250))
     // time.Sleep(amt)
-    fmt.Println(msg)
+    fmt.Println(msgs)
 }
 
+// read from stdin and send line of text to channel in
 func reader() (in chan string) {
     in = make(chan string)
     go func() {
@@ -103,4 +108,27 @@ func reader() (in chan string) {
         close(in)
     } ()
     return
+}
+
+func initTicker() *ticker {
+    t := &ticker{}
+    t.sender = time.Tick(time.Second)
+    t.recievers = make([](chan time.Time), 0)
+
+    go func() {
+        for now := range t.sender {
+            for _, rec := range t.recievers {
+                // don't put this in an annoymous function
+                rec <- now
+            }
+        }
+    }()
+
+    return t
+}
+
+func (t *ticker) Register() (chan time.Time) {
+    c := make(chan time.Time)
+    t.recievers = append(t.recievers, c)
+    return c
 }
